0ed54ed32d0e885843bc0093e3f6096432b1bfeb
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-24 20:33:41 +0530

    changes

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import "../styles/Child.css";
 const Selection = ({ applyColor, index, selectNextBackground, backGround }) => {
+  const change = () => {
+    applyColor(index);
+  };
   return (
-    <div
-      className="fix-box"
-      onClick={() => applyColor(index)}
-      style={backGround[index]}
-    >
+    <div className="fix-box" onClick={change} style={backGround[index]}>
       <h2 className="subheading">Selection</h2>
     </div>
   );
